e5ad874aee3a122c54ff27b99ff7306de61ed180
HokoNozawa <111749981+hnoza@users.noreply.github.com>
2022-08-23 22:52:00 +0900

    Chapter6,7を追加

---
+++ src/Chapter06.java
@@ -0,0 +1,30 @@
+
+public class Chapter06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		long long_a = 10000000000L;
+		System.out.println(long_a);
+		
+		double double_a = 3.14;
+		System.out.println(double_a);
+		
+		boolean boolean_a = true;
+		System.out.println(boolean_a);
+		
+		Integer integer_a = 100;
+		System.out.println(integer_a);
+		
+		Float float_a = 1.4142F;
+		System.out.println(float_a);
+		
+		Character caracter_a = 'う';
+		System.out.println(caracter_a);
+		
+		int[] int_array = new int[] {0,10,20,30,40};
+		System.out.println(int_array[3]);
+		
+
+	}
+
+}
