f00e971296a949a2e42e305b70f590f59c532d9f
HokoNozawa <111749981+hnoza@users.noreply.github.com>
2022-08-23 18:15:39 +0900

    Chapter05追加

---
+++ src/Chapter05.java
@@ -0,0 +1,15 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		String name = "Nozawa Hoko";
+		System.out.println(name);
+		
+		name = "aiueo";
+		System.out.println(name);
+		
+		final int NUMBER = 3;
+		System.out.println(NUMBER);
+	}
+
+}
