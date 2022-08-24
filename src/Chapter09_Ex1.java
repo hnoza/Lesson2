8b14de87012cba541aa3a2bbb4136500f0ba6a1e
HokoNozawa <111749981+hnoza@users.noreply.github.com>
2022-08-24 16:01:07 +0900

    Chapter9追加

---
+++ src/Chapter09_Ex1.java
@@ -0,0 +1,17 @@
+
+public class Chapter09_Ex1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		for (int a = 1; a < 10; a++) {
+			for (int b = 1; b < 10; b++) {
+				System.out.print(a + "*" + b + "=" + a*b + " ");
+			}
+			// 段ごとに改行
+			System.out.println();
+		}
+
+	}
+
+}
